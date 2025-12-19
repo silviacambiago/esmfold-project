f5cedd3c726aa17570aac2d1f0033f04692323f2
silviac02 <silviaco2@zedat.fu-berlin.de>
2025-12-19 02:49:31 +0100

    cleaned

--- check.py
+++
@@ -1,7 +0,0 @@
-from esm import pretrained
-import torch
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-model = pretrained.esmfold_v1().to(device)
-print("model first param device:", next(model.parameters()).device)
